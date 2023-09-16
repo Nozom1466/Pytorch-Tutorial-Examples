9436c37a4ed9c6972cdddaa350ce6d075d2ad0c8
Nozom1466 <98678848+Nozom1466@users.noreply.github.com>
2023-09-16 12:43:28 +0800

    object detection

---
+++ default_settings/mpl_figure_settings.py
@@ -0,0 +1,12 @@
+
+def set_params(plt):
+    plt.rcParams['figure.facecolor'] = 'white'
+    plt.rcParams['axes.facecolor'] = 'white'
+    plt.rcParams['axes.grid'] = True
+    plt.rcParams['grid.color'] = 'grey'
+    plt.rcParams['grid.alpha'] = 0.2
+    plt.rcParams['figure.figsize'] = (10, 7)
+    plt.rcParams['font.size'] = 16
+    plt.rcParams['savefig.bbox'] = 'tight'
+    plt.rcParams['savefig.dpi'] = 500
+    plt.rcParams['savefig.transparent'] = True
